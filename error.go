9a3cb9d83f020affe8167617e9c7532a9d7b0043
Sandeep Jadoonanan <sandeep+tech@sandeep.net>
2024-03-05 17:40:50 -0500

    Work on docstrings and README.

--- error.go
+++ error.go
@@ -2,16 +2,22 @@ package rio
 
 import "net/http"
 
+// AppError is custom error type for Http/Json errors.
 type AppError struct {
 	Message string
 	Status  int
 	IsJson  bool
 }
 
+// Error satisfies the error interface.
 func (a *AppError) Error() string {
 	return a.Message
 }
 
+// WriteTo writes the AppError to the given ResponseWriter.
+//
+// If the AppError is Json, then a json object containing the message will be written.
+// If the AppError is not Json, then a plain text message will be written.
 func (a *AppError) WriteTo(w http.ResponseWriter) error {
 	if a.IsJson {
 		return writeJson(w, a.Message, a.Status)
@@ -21,6 +27,7 @@ func (a *AppError) WriteTo(w http.ResponseWriter) error {
 	return nil
 }
 
+// HttpError constructs and returns an Http *AppError.
 func HttpError(message string, status int) *AppError {
 	return &AppError{
 		Message: message,
@@ -29,6 +36,7 @@ func HttpError(message string, status int) *AppError {
 	}
 }
 
+// JsonError constructs and returns a Json *AppError.
 func JsonError(message string, status int) *AppError {
 	return &AppError{
 		Message: message,
--- logger.go
+++ logger.go
@@ -17,38 +17,32 @@ import (
 
 var defaultLogger = NewLogger(os.Stdout)
 
-// Logger sets the default logger to the provided slog.Logger.
-// .
+// Logger sets the default logger to the given slog.Logger.
 func Logger(l *slog.Logger) {
 	defaultLogger = l
 }
 
-// NewLogger constructs a new *slog.Logger with the given writer.
-// .
+// NewLogger constructs and returns a new *slog.Logger.
 func NewLogger(w io.Writer) *slog.Logger {
 	return slog.New(slog.NewTextHandler(w, nil))
 }
 
-// LogDebug logs a debug message using the default logger.
-// .
+// LogDebug logs a debug message.
 func LogDebug(msg string, attrs ...slog.Attr) {
 	defaultLogger.LogAttrs(context.Background(), slog.LevelDebug, msg, attrs...)
 }
 
-// LogDebug logs an info message using the default logger.
-// .
+// LogInfo logs an info message.
 func LogInfo(msg string, attrs ...slog.Attr) {
 	defaultLogger.LogAttrs(context.Background(), slog.LevelInfo, msg, attrs...)
 }
 
-// LogDebug logs a warning message using the default logger.
-// .
+// LogWarn logs a warning message.
 func LogWarn(msg string, attrs ...slog.Attr) {
 	defaultLogger.LogAttrs(context.Background(), slog.LevelWarn, msg, attrs...)
 }
 
-// LogDebug logs an error using the default logger.
-// .
+// LogError logs an error.
 func LogError(err error, attrs ...slog.Attr) {
 	defaultLogger.LogAttrs(context.Background(), slog.LevelError, err.Error(), attrs...)
 }
